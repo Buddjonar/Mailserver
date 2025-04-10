8e354c3df04946f23401c37e584f3d56f89bc509
Andreas <h23anber@du.se>
2025-04-10 09:32:49 +0200

    asd

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+
+require("dotenv").config();
+//Laddar in nodemailer som gör de möjligt att skicka mejl via node.js
 const nodemailer = require("nodemailer");
 
+//Skapar upp en koppling mellan gmail och nodemailer
+//Hämtar användarnamn och app-lösenord ifrån en miljövariabel
+//Vi gör så för att förhindra att andra kan komma åt dessa uppgifter
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -8,6 +14,7 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+//Själva mejlet som ska skickas iväg
 const mailOptions = {
   from: process.env.EMAIL_USER,
   to: "mottagare@example.com",
@@ -15,6 +22,8 @@ const mailOptions = {
   text: "Detta är ett testmeddelande från en Glitch-app."
 };
 
+//Här så försöker vi skicka iväg det
+//Stöter vi på error så skriver vi ut det i konsolen
 transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     return console.log(error);
